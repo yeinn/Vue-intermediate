aaa29a340ad82f593e707454a03f287e0f5e7e08
최 예인 <yichoi@jinhak.com>
2021-05-18 18:00:12 +0900

    Todo Input  구현

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <header>
+        <h1>TO DO it!</h1>
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+// 현재파일에만 적용
+<style scoped>
+    h1{
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        List
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
